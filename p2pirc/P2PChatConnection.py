477ad8ae8076a0dfd1a8ef6dd79f24de4ef729cc
Ecl1psed276 <36321263+briankehrig@users.noreply.github.com>
2020-03-30 17:53:14 -0600

    Added refactored version of p2pirc-bad

--- p2pirc/P2PChatConnection.py
+++ p2pirc/P2PChatConnection.py
@@ -1,10 +1,145 @@
+import select, socket, time
 
+def send(sock, string):
+    data = string.encode('utf-8')
+    lendata = len(data).to_bytes(4, 'big')
+    sock.sendall(lendata + data)
 
-
+def receive(sock):
+    length = int.from_bytes(sock.recv(4), 'big')
+    return sock.recv(length).decode('utf-8')
 
 class P2PChatConnection:
-	def __init__(self,local_port_min,key_file):
-		print("Connection object initialized.")
-		
-	def connect(self,IP,port):
-		print("Connection established.")
+    def __init__(self, local_port_min, key_file):
+        print("Connection object initialized.")
+        ''' self.connList is a list of sockets we use to communicate with other
+        people in the chat room '''
+        self.listenerIP = '127.0.0.1'
+        self.listenerPort = local_port_min
+        self.connList = []
+            
+    def connect(self, IP, port):
+        '''
+        This function should be run to connect to an already existing room.
+        It assumes that we are not already connected to a room.
+        '''
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((IP, port))
+            print('Connected')
+
+            # 'addr' is the ip/port of the socket we need to connect to
+            addr = eval(receive(s))
+
+        # 'firstsock' will be the first element of self.connList.
+        firstsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        firstsock.connect(addr)
+
+        # The person we connect to will now tell everyone else to create
+        # a socket for us to use. Then that person will send us the
+        # addresses of the sockets they created.
+        addrlist = eval(receive(firstsock))
+
+        self.connList = [firstsock]
+        for addr in addrlist:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(addr)
+            self.connList.append(sock)
+        
+        print("Connection established.")
+        try:
+            self.createListener()
+            self.mainLoop()
+        finally:
+            print('Closing our listener')
+            self.listener.close()
+            print('Closing our connection ports')
+            for conn in self.connList:
+                conn.close()
+            print('Done!')
+
+    def createNewRoom(self):
+        self.connList = []
+
+        try:
+            self.createListener()
+            self.mainLoop()
+        finally:
+            print('Closing our listener')
+            self.listener.close()
+            print('Closing our connection ports')
+            for conn in self.connList:
+                conn.close()
+            print('Done!')
+
+    def createListener(self):
+        '''
+        Creates a new listener. New clients can connect to this listener to join
+        the room that we are currently in.
+        '''
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener.bind((self.listenerIP, self.listenerPort))
+        self.listener.listen()
+        print('Listening on', self.listener.getsockname())
+
+    def mainLoop(self):
+        while True:
+            time.sleep(0.01)
+            # The other two values from this function are writable and errored.
+            # We might use the third one in the future to detect if someone has
+            # disconnected (which would mean that their connection socket has closed)
+            readable, _, _ = select.select(self.connList+[self.listener], [], [])
+
+            for sock in readable:
+                if sock is self.listener:
+                    # A new client has tried to connect to our listener
+                    s, addr = self.listener.accept()
+                    with s:
+                        print('Received new client at', addr)
+                        # Start listening for new clients right away
+                        self.listener.listen()
+                        # Create a new socket - this socket will be used for
+                        # communication with the new client.
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tmpsock:
+                            tmpsock.bind(('127.0.0.1', 0))
+                            tmpsock.listen()
+                            send(s, str(tmpsock.getsockname()))                   
+                            newsock, addr = tmpsock.accept()
+                            # We will eventually add newsock to connList, but not right
+                            # away, since we need to use connList first
+
+                    # Tell everyone else to make a new socket for their communication
+                    # with the new client
+                    for conn in self.connList:
+                        send(conn, 'makenewsock')
+                    print('Told everyone to make a new socket for the client')
+
+                    # Everyone else will send a response back containing the address
+                    # of the new socket they made
+                    newsocks = []
+                    for conn in self.connList:
+                        newsocks.append(eval(receive(conn)))
+                    print('Received the new socket addresses')
+
+                    self.connList.append(newsock)
+                    send(newsock, str(newsocks))
+                    print('Done!')
+                    
+                else:
+                    # Received data from one of our sockets
+                    data = receive(sock)
+                    if data == 'makenewsock':
+                        # Someone else (call this person C) is telling us to
+                        # make a new socket for a new client to communicate with us
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tmpsock:
+                            tmpsock.bind(('127.0.0.1', 0))
+                            tmpsock.listen()
+                            print('New client has joined. Communication port has address',
+                                  tmpsock.getsockname())
+
+                            # Send C the address of the new socket we created.
+                            # C will then send this to the new client, who will connect
+                            # to our socket.
+                            send(sock, str(tmpsock.getsockname()))
+                            newsock, addr = tmpsock.accept()
+                            self.connList.append(newsock)
--- p2pirc/__main__.py
+++ p2pirc/__main__.py
@@ -4,48 +4,53 @@ import socket
 from .P2PChatConnection import P2PChatConnection
 
 DEFAULT_PORT_MIN = 38500 #Needs this port for the server, and one more port for each person in the chat
-
+# Port 0 means that we let the OS choose a free port
+DEFAULT_PORT_MIN = 0
 
 def main():
-	print("Parsing arguments...")
-	parser = ArgumentParser(description="A peer-to-peer group chat proof of concept.")
-	
-	parser.add_argument('--ip',metavar='X.X.X.X',default='127.0.0.1',
-						help="The IP of one of the people currently in the group chat")
-	parser.add_argument('--port',type=int,metavar='N',default=DEFAULT_PORT_MIN,
-						help="The port of that person's server you want to connect to")
-	parser.add_argument('--local-port',type=int,metavar='N',default=DEFAULT_PORT_MIN,
-						help="The port to host your connection server on")
-	parser.add_argument('--key-file',metavar='*.json',default='id-rsa.json',
-						help="The file containing the key database")
-	
-	args = parser.parse_args()
-	
-	
-	#VALIDATION - maybe also check if the ports are available?
-	
-	try:
-		socket.inet_aton(args.ip) #Only support ipv4 for now
-	except socket.error:
-		print("Invalid IP %s." %args.ip)
-	
-	if args.port<1 or args.port>65535:
-		print("Invalid port %d." %args.port)
-		return
-		
-	if args.local_port<1 or args.local_port>65535:
-		print("Invalid local port %d." %args.local_port)
-		return
-		
-	try:
-		fp = open(args.key_file, "a+")  #Creates file if it doesn't exist
-		fp.close()
-	except IOError:
-		print("Invalid key file %s." %args.key_file)
-		return
-		
-	
-	#Everything is good at this point, start the class
-	
-	con = P2PChatConnection(args.local_port,args.key_file)
-	con.connect(args.ip,args.port)
+        print("Parsing arguments...")
+        parser = ArgumentParser(description="A peer-to-peer group chat proof of concept.")
+        
+        parser.add_argument('--ip',metavar='X.X.X.X',default='127.0.0.1',
+                                                help="The IP of one of the people currently in the group chat")
+        parser.add_argument('--port',type=int,metavar='N',default=DEFAULT_PORT_MIN,
+                                                help="The port of that person's server you want to connect to")
+        parser.add_argument('--local-port',type=int,metavar='N',default=DEFAULT_PORT_MIN,
+                                                help="The port to host your connection server on")
+        parser.add_argument('--key-file',metavar='*.json',default='id-rsa.json',
+                                                help="The file containing the key database")
+        parser.add_argument('--new',action='store_true',help="The file containing the key database")
+        
+        args = parser.parse_args()
+        
+        
+        #VALIDATION - maybe also check if the ports are available?
+        
+        try:
+                socket.inet_aton(args.ip) #Only support ipv4 for now
+        except socket.error:
+                print("Invalid IP %s." %args.ip)
+        
+        if args.port<0 or args.port>65535:
+                print("Invalid port %d." %args.port)
+                return
+                
+        if args.local_port<0 or args.local_port>65535:
+                print("Invalid local port %d." %args.local_port)
+                return
+                
+        try:
+                fp = open(args.key_file, "a+")  #Creates file if it doesn't exist
+                fp.close()
+        except IOError:
+                print("Invalid key file %s." %args.key_file)
+                return
+                
+        
+        #Everything is good at this point, start the class
+        
+        con = P2PChatConnection(args.local_port,args.key_file)
+        if args.new:
+                con.createNewRoom()
+        else:
+                con.connect(args.ip,args.port)
