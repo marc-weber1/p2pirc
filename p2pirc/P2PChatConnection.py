6eadcd8e17a0c6cf800185e6862cb999019625f0
check080 <mwiloveicecream565@gmail.com>
2020-03-30 15:46:07 -0600

    Heavily improved command-line interface.

---
+++ p2pirc/P2PChatConnection.py
@@ -0,0 +1,10 @@
+
+
+
+
+class P2PChatConnection:
+	def __init__(self,local_port_min,key_file):
+		print("Connection object initialized.")
+		
+	def connect(self,IP,port):
+		print("Connection established.")
